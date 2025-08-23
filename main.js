cbf7bbc54a9938ad6c38e6a239a0fd85a0d83b40
rimardev <catamarce.bs@gmail.com>
2025-08-23 11:55:08 -0400

    extructura base

---
+++ main.js
@@ -0,0 +1,19 @@
+//variables
+
+//cargar local storage
+
+//guardar local storage
+
+//renderizacion
+
+//agregar proyecto
+
+//seleccion proyecto
+
+//accion botones play - stop
+
+//guardar estados sesion actual
+
+//calcular tiempo trascurrido
+
+//borrar proyecto
