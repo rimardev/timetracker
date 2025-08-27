07ba4a5e12cb3ae05f74c83181cb81b18902bad3
rimardev <catamarce.bs@gmail.com>
2025-08-26 22:16:03 -0400

    inicio logica, carga array de local storage + carga objeto basico

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 //variables
+const nuevoProyecto = document.getElementById('inputProyecto');
+const btnAgregar = document.getElementById('btnAgregar');
+let id = 0;
 
-//cargar local storage
+//cargar local storage 
+let arrayPoyectos = JSON.parse(localStorage.getItem('proyectos')) || [];
+console.log(arrayPoyectos);
 
 //guardar local storage
+function guardarProyecto(proyecto) {
+  localStorage.setItem('proyectos', JSON.stringify(proyecto));
+  console.log(arrayPoyectos);  
+};
 
 //renderizacion
 
 //agregar proyecto
+btnAgregar.addEventListener('click', () => {
+  console.log(nuevoProyecto.value);
+  let data = {
+    nombre: nuevoProyecto.value,
+    estado: false
+  };
+  arrayPoyectos.unshift(data);
+  guardarProyecto(arrayPoyectos);
+});
 
 //seleccion proyecto
 
